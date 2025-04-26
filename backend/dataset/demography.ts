e8e2c2932ef18cec301e6e7c61be211445c1f2b9
Ludvig Zapffe <ludvigz@stud.ntnu.no>
2025-04-26 17:42:22 +0200

    Endring av demography

--- backend/dataset/demography.ts
+++ backend/dataset/demography.ts
@@ -9,37 +9,37 @@ export const demographics: Demography[] = [
   {
     id: 1,
     ageGroup: "Barn",
-    marketShare: 0.2,
+    marketShare: 0.1,
     score: 45,
   },
   {
     id: 2,
     ageGroup: "Tenåringer",
-    marketShare: 0.3,
+    marketShare: 0.15,
     score: 60,
   },
   {
     id: 3,
     ageGroup: "Unge voksne",
-    marketShare: 0.4,
+    marketShare: 0.25,
     score: 75,
   },
   {
     id: 4,
     ageGroup: "Studenter",
-    marketShare: 0.5,
+    marketShare: 0.05,
     score: 80,
   },
   {
     id: 5,
     ageGroup: "Familier",
-    marketShare: 0.6,
+    marketShare: 0.30,
     score: 70,
   },
   {
     id: 6,
     ageGroup: "Seniorer",
-    marketShare: 0.7,
+    marketShare: 0.15,
     score: 50,
   },
 ];
